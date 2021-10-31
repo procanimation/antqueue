705e163c192b356b024d09b91fa55eb922db6999
shubham <shubhamshandilya198@gmail.com>
2021-10-31 11:49:16 +0530

    Commit Initialization

---
+++ README.md
@@ -0,0 +1,3 @@
+This Animation is about a problem where N ant from both side are coming, when the two ant collide they change their direction.
+Some modification can possible:-
+Change in speed.
---
+++ sketch.js
@@ -0,0 +1,50 @@
+let n;
+let m;
+let arra;
+let arrb;
+let count;
+function setup() {
+   createCanvas(480,480);
+   
+   n = 3;
+   m = 3;
+   arra = new Array();
+   arrb = new Array();
+   for(let i=0;i<n;i++){
+      arra.push(int(random(0, width/2)));
+      arrb.push(-1);
+   }
+
+   for(let i=0;i<m;i++){
+      arra.push(-1*int(random(0, width/2)));
+      arrb.push(1);
+   }
+   count = 0;
+}
+function draw(){
+   background(0);
+   translate(height/2, width/2);
+   stroke(1);
+   stroke(0, 230, 220, 1);
+   fill(0, 255, 0);
+   line(-height/2, 0, height/2, 0);
+   for(let i=0;i<n+m;i++){
+      ellipse(arra[i], 0, 10, 10);
+   }
+   for(let i=0;i<n+m;i++){
+      for(let j=i+1;j<n+m;j++){
+         
+         if(arra[i]==arra[j]){
+
+            arrb[i]*= -1;
+            arrb[j]*= -1;
+            //count = 1;
+            console.log(count);
+         }
+      }
+   }
+   for(let i=0;i<n+m;i++)
+      arra[i] += 1*arrb[i];
+   //console.log(count);
+   
+}
